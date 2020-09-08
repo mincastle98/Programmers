29068c3d3c259a19bf4d3f0afceca0ae8649a2c4
Choiyunh_ <svs101@naver.com>
2020-09-08 12:05:58 +0900

    20/09/08 solved fakeExam

---
+++ fakeExam/uno.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> answers) {
+    vector<int> answer;
+    vector<int> num(3);
+    int max = 0;
+
+    int first[5] = {1, 2, 3, 4, 5};
+    int second[8] = {2, 1, 2, 3, 2, 4, 2, 5};
+    int third[10] = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+
+    for (int i = 0; i < answers.size(); i++) {
+        if (first[i % 5] == answers[i])
+            num[0]++;
+        if (second[i % 8] == answers[i])
+            num[1]++;
+        if (third[i % 10] == answers[i])
+            num[2]++;
+    }
+    max = num[0];
+    if (num[1] < num[2]) {
+        if (max < num[2])
+            max = num[2];
+    } else if (max < num[1])
+        max = num[1];
+
+    for (int i = 0; i < 3; i++) {
+        if (num[i] == max) answer.push_back(i + 1);
+    }
+    return answer;
+}
+
+int main(){
+    vector<int> answers = {1,3,2,4,2};
+    vector<int> res = solution(answers);
+
+    for(int i=0;i<res.size();i++)
+        cout<< res[i] << ' ';
+
+    return 0;
+}
