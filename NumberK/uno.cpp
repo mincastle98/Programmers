d855229101c740b0f240b4113d2eb30731988c78
Choiyunh_ <svs101@naver.com>
2020-09-16 18:30:53 +0900

    20/09/16 solved numberK - uno

---
+++ NumberK/uno.cpp
@@ -0,0 +1,32 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+vector<int> solution(vector<int> array, vector<vector<int> > commands) {
+	vector<int> answer;
+	vector<int> temp;
+
+	for (size_t i = 0; i < commands.size(); i++) {
+		temp.assign(array.begin() + (commands[i][0] - 1), array.begin() + (commands[i][1]));
+		sort(temp.begin(), temp.end());
+		answer.push_back(temp[commands[i][2] - 1]);
+	}
+
+	return answer;
+}
+
+int main(){
+    vector<int> a = {1, 5, 2, 6, 3, 7, 4};
+    vector<vector<int> > c = {{2,5,3},{4,4,1},{1,7,3}};
+
+    vector<int> res = solution(a,c);
+
+    for(int i=0; i<res.size(); i++){
+        cout << res[i] << endl;
+    }
+    
+    return 0;
+}
