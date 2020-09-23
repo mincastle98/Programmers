b2c3857e14641f213f5710d0fbf0d3bc9fd6bca1
LeeKangh22 <dlrkdgml1998@naver.com>
2020-09-23 20:37:23 +0900

    2020/09/23 solved by Leekh

---
+++ Week 3/secretMap/kh.cpp
@@ -0,0 +1,56 @@
+#include <string>
+#include <vector>
+#include<iostream>
+
+using namespace std;
+
+vector<string> solution(int n, vector<int> arr1, vector<int> arr2) {
+    vector<string> answer;
+    vector<string>arr1_s;
+    vector<string>arr2_s;
+    string temp;
+    int sq = 1;
+
+    for (int i = 0; i < n; i++) {
+        sq *= 2;
+    }
+
+    for (int j = 0; j < arr1.size(); j++) {
+        temp.clear();
+        for (int i = n - 1; i >= 0; i--)
+        {
+            if (arr1[j] & (1 << i))
+                temp += '#';
+            else
+                temp += ' ';
+        }
+        arr1_s.push_back(temp);
+    }
+
+    for (int j = 0; j < arr2.size(); j++) {
+        temp.clear();
+        for (int i = n-1; i >= 0; i--)
+        {
+            if (arr2[j] & (1 << i))
+                temp += '#';
+            else
+                temp += ' ';
+        }
+        arr2_s.push_back(temp);
+    }
+
+    for (int i = 0; i < arr1_s.size(); i++) {
+        cout << arr1_s[i] << endl;
+    }
+    for (int i = 0; i < arr2_s.size(); i++) {
+        cout << arr2_s[i] << endl;
+    }
+    
+    return answer;
+}
+
+int main() {
+    vector<int>arr1 = { 9, 20, 28, 18, 11 };
+    vector<int>arr2 = { 30, 1, 21, 17, 28 };
+    vector<string>answer = solution(5, arr1, arr2);
+}
