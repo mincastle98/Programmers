e76cd6bcd1e21236e9e4f614bdc2cb222db53d8a
HyewonKkang <gpffps369@gmail.com>
2020-10-02 22:39:31 +0900

    20/10/02 solved stockPrice - hyewon

---
+++ Week 4/stockPrice/hyewon.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> prices) {
+	vector<int> answer;
+	for (int i = 0; i < prices.size(); i++) {
+		int cnt = 0;
+		for (int j = i + 1; j < prices.size(); j++) {
+			if (prices[i] > prices[j]) { 
+				cnt++;
+				break;
+			}
+			else cnt++;
+		}
+		answer.push_back(cnt);
+	}
+	return answer;
+}
