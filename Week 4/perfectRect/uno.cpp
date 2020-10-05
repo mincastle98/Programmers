67c144687cbc75b46125dcdcd9e821ab73c39911
Choiyunh_ <svs101@naver.com>
2020-10-05 16:42:36 +0900

    20/10/05 solving perfectRect - uno

--- Week 4/perfectRect/uno.cpp
+++ Week 4/perfectRect/uno.cpp
@@ -2,23 +2,26 @@
 
 using namespace std;
 
-long long solution(int w, int h) {	
+long long solution(int w, int h) {
 	long long answer = 0;
 
 	long long w_ = w;
 	long long h_ = h;
 
-	for (int x = 1; x < w; x++) {
-		for (int y = 1; y < h; y++) {
-			if (w_ * y <= -h_ * x + (w_ * h_))
-				answer++;
-			else
-				break;
-		}
-	}
+	for (int y = 1; y < h; y++) 
+		answer += (double)((w_ * h_) - (w_ * y)) / h_;	
 
 	return answer * 2;
+}
 
+	//for (int x = 1; x < w; x++) {
+	//	for (int y = 1; y < h; y++) {
+	//		if (w_ * y <= -h_ * x + (w_ * h_))
+	//			answer++;
+	//		else
+	//			break;
+	//	}
+	//}
 
 	////w = 8, h = 12의 원래 직사각형에서는 정사각형 96개 사용가능(8*12)
 	////대각선으로 그으면 총 16개를 사용할 수 없음
@@ -54,7 +57,7 @@ long long solution(int w, int h) {
 	//	answer = ((long long)w * h) - (min(w, h)*2);
 
 	//return answer;
-}
+
 
 int main() {
 	int w = 8, h = 12;
