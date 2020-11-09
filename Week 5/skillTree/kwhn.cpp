2a42499d6fe702a7ffc8bc2b6d706af9f8794b1a
김응관 <40496065+alpha-kwhn@users.noreply.github.com>
2020-11-09 18:48:01 +0900

    Update kwhn.cpp

--- Week 5/skillTree/kwhn.cpp
+++ Week 5/skillTree/kwhn.cpp
@@ -1,124 +1,84 @@
-#include<string>
-#include<vector>
-#include<stack>
-#include<algorithm>
-#include<iostream>
+#include <string>
+#include <vector>
+#include <stack>
+#include <algorithm>
+#include <queue>
 using namespace std;
 
 int solution(string skill, vector<string> skill_trees)
 {
 	int answer = 0;
-	int end = skill.length(); //스킬의 길이
-	int tmp_len; //스킬트리 문자열의 길이
-	stack<char> tmp; //스킬의 길이 뒤에서 부터 저장할 stack
-	stack<char> ptr;
-	int count = 0;
-
-	for (int i = 0; i < end; i++)
-		ptr.push(skill[i]);
+	int check = 0;
+	queue<int> test;
+	queue<int> test2;
+	for (int t = 0; t < skill.length(); t++)
+		test2.push(skill[t]);
 
 	for (int j = 0; j < skill_trees.size(); j++)
 	{
-		for (int k = 0; k < skill_trees[j].length(); k++)
+		for (int i = 0; i < skill_trees[j].length(); i++)
 		{
 			for (int p = 0; p < skill.length(); p++)
 			{
-				if (skill_trees[j][k] == skill[p])
-				{
-					tmp.push(skill[p]);
-				}
+				if (skill_trees[j][i] == skill[p])
+					test.push(skill[p]);
 			}
 		}
-		if (tmp.size() == 0)
-		{
+		if (test.size() == 0)
 			answer++;
-		}
-		else
+		else if (test.front() != test2.front())
 		{
-			while (!tmp.empty() && (end - 1) >= 0)
+			while (!test.empty())
+				test.pop();
+		}
+		else if(test.front() == test2.front())
+		{
+			test.pop();
+			test2.pop();
+			while (!test.empty() && !test2.empty())
 			{
-				if (tmp.top() == skill[end - 1])
+				if (test.front() != test2.front())
 				{
-					ptr.pop();
-					tmp.pop();
-					count = 1;
+					break;
 				}
-				else if (tmp.top() != skill[end - 1])
+				else if (test.front() == test2.front())
 				{
-					if (count == 0)
-						ptr.pop();
-					else if (count == 1)
-						tmp.pop();
+					test.pop();
+					test2.pop();
 				}
 			}
-			if (tmp.empty() && ptr.empty())
+			if (test.empty() && test2.empty())
 			{
 				answer++;
-				for (int i = 0; i < end; i++)
-					ptr.push(skill[i]);
-				count = 0;
+				for (int i = 0; i < skill.length(); i++)
+					test2.push(skill[i]);
 			}
-			else if (tmp.empty() && !ptr.empty())
+			else if (!test.empty() && test2.empty())
 			{
-				end = skill.length();
-				while (!ptr.empty())
-					ptr.pop();
-				for (int i = 0; i < end; i++)
-					ptr.push(skill[i]);
-				count = 0;
+					answer++;
+					while (!test.empty())
+						test.pop();
+					for (int t = 0; t < skill.length(); t++)
+						test2.push(skill[t]);
 			}
-			else 
+			else if (test.empty() && !test2.empty())
 			{
-				end = skill.length();
-				while (!tmp.empty())
-					tmp.pop();
-				count = 0;
+				answer++;
+				while (!test2.empty())
+					test2.pop();
+				for (int t = 0; t < skill.length(); t++)
+					test2.push(skill[t]);
+			}
+			else
+			{
+				while (!test2.empty())
+					test2.pop();
+				for (int t = 0; t < skill.length(); t++)
+					test2.push(skill[t]);
+				while (!test.empty())
+					test.pop();
 			}
 		}
 	}
 	return answer;
 }
-
-void main()
-{
-	string skill = "CBD";
-	vector<string> skill_trees;
-	skill_trees.push_back("BACDE");
-	skill_trees.push_back("CBADF");
-	skill_trees.push_back("AECB");
-	skill_trees.push_back("BDA");
-	int k = solution(skill, skill_trees);
-	cout << k << endl;
-}
-
-/*
-while ((end-1) != 0 && !tmp.empty())
-{
-if (tmp.top() == skill[end - 1])
-{
-tmp.pop();
-end--;if (tmp.top() != skill[end - 1])
-break;
-
-}
-else if (tmp.top() != skill[end - 1])
-end--;
-}
-if ((end-1 == 0) && tmp.empty())
-{
-answer++;
-end = skill.length();
-}
-else if ((end - 1) != 0 && tmp.empty())
-{
-end = skill.length();
-answer++;
-}
-else
-{
-end = skill.length();
-while (!tmp.empty())
-tmp.pop();
-}
-}
-*/
