b95ac98a3bd54a973e807cb2e1d513793e9b3ba3
mincastle98 <alstjd98522@naver.com>
2020-12-31 17:43:34 +0900

    20/12/31 solving PrimeNumber - ms

--- Week 9/PrimeNumber/ms.cpp
+++ Week 9/PrimeNumber/ms.cpp
@@ -6,29 +6,27 @@ using namespace std;
 int solution(vector<int> nums) {
     int answer = -1;
     vector<int> sums;
-    int primeNum[] = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37};
 
-    for(int i = 0; i < nums.size(); i++) {
-        for(int j = i; j < nums.size(); j++) {
-            for(int k = j; k < nums.size(); k++) {
-                if (i == j) break;
-                if (i == k || j == k) continue;
+    for (int i = 0; i < nums.size(); i++) {
+        for (int j = i + 1; j < nums.size(); j++) {
+            for (int k = j + 1; k < nums.size(); k++) {
                 sums.push_back(nums[i] + nums[j] + nums[k]);
             }
         }
     }
 
-    for (int i : primeNum) {
-        for (int j = 0; j < sums.size(); j++) {
-            if (sums[j] % i == 0 && sums[j] / i != 1) {
-                cout << sums[j] << endl;
-                sums.erase(sums.begin() + j);
-                j--;
+    for (int i : sums) {
+        int flag = 1;
+        for (int j = 2; j < i; j++) {
+            if (i % j == 0) {
+                flag = 0;
+                break;
             }
         }
+        if (flag) answer++;
     }
 
-    answer = sums.size();
+    if (answer != -1) answer++;
 
     return answer;
 }
