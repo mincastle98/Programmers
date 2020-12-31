ea91b50908c81eed38edaaaa9f0db13dde4b45d0
mincastle98 <alstjd98522@naver.com>
2020-12-31 18:04:45 +0900

    20/12/31 solved LargestNumber - ms

--- Week 9/LargestNumber/ms.cpp
+++ Week 9/LargestNumber/ms.cpp
@@ -4,58 +4,23 @@
 
 using namespace std;
 
-bool compare(vector<int> a, vector<int> b) {
-    for (int i = 0; i < min(a.size(), b.size()); i++) {
-        if (a[i] > b[i]) return true;
-        else if (a[i] < b[i]) return false;
-        else {
-            if (min(a.size(), b.size()) == a.size() && i == a.size() - 1) {
-                if (b[i + 1] > b[i]) return false;
-                else return true;
-            } else if (min(a.size(), b.size()) == b.size() && i == b.size() - 1) {
-                if (a[i + 1] > a[i]) return true;
-                else return false;
-            }
-            if (a[i + 1] > a[i]) {
-                if (b[i + 1] > b[i])
-                    continue;
-                else return true;
-            } else if (b[i + 1] > b[i]) {
-                if (a[i + 1] > a[i])
-                    continue;
-                else return false;
-            } else continue;
-        }
-    }
+bool compare(string a, string b) {
+    return a + b > b + a;
 }
 
 string solution(vector<int> numbers) {
     string answer = "";
 
-    vector<vector<int>> nums;
-    for (int i : numbers) {
-        vector<int> tmp;
-        while (i >= 10) {
-            tmp.push_back(i % 10);
-            i /= 10;
-        }
-        tmp.push_back(i);
-        reverse(tmp.begin(), tmp.end());
-        nums.push_back(tmp);
-    }
+    vector<string> nums;
+    for (int i : numbers)
+        nums.push_back(to_string(i));
 
     sort(nums.begin(), nums.end(), compare);
 
-    for (vector<int> i : nums) {
-        for (int j : i) {
-            answer += j + '0';
-        }
+    for (string i : nums) {
+        answer += i;
     }
+    if(answer[0] == '0') answer = "0";
 
     return answer;
-}
-
-int main(void) {
-    solution({3, 30, 34, 5, 9});
-    return 0;
 }
