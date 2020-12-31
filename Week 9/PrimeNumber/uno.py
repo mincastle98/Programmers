06a052da1240c59aa05401a55f03c487f18b21b2
Choiyunh_ <svs101@naver.com>
2020-12-31 17:04:16 +0900

    20/12/31 solved PrimeNumber with python3

--- Week 9/PrimeNumber/uno.py
+++ Week 9/PrimeNumber/uno.py
@@ -1,11 +1,31 @@
+from itertools import combinations
+
+
+def isPrime(num):
+    flag = 1
+
+    for i in range(2, num):
+        if (num % i == 0):
+            flag = 0
+            break
+
+    if (flag == 1):
+        return True
+    else:
+        return False
+
+
 def solution(nums):
-    answer = -1
+    answer = 0
+    comList = list(combinations(nums, 3))
 
-
-
-
+    for item in comList:
+        if (isPrime(sum(item))):
+            answer += 1
     return answer
 
 
- # [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-print('Hello Python')
+# 5C3 -> 5*4*3/3*2*1 * 1/2
+
+
+print(solution([1, 2, 7, 6, 4]))
