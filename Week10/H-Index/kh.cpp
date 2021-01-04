c2549eac4261eb5b29392806c136b48a7d9873f9
KANG HEE LEE <67950809+LeeKangh22@users.noreply.github.com>
2021-01-04 18:09:00 +0900

    Update kh.cpp

--- Week10/H-Index/kh.cpp
+++ Week10/H-Index/kh.cpp
@@ -7,28 +7,16 @@ using namespace std;
 
 int solution(vector<int> citations) {
     int answer = 0;
-    int temp = 0;
     vector<int>size;
     sort(citations.begin(),citations.end());
 
-    int h;
+    int h = 0;
     int front, end, length;
-    /*
-    for (int i = 0; i < citations.size(); i++) {
-        h = citations[i];
-        for (int j = 0; j < citations.size(); j++) {
-            if (h <= citations[j]) {
-                temp++;
-                if (temp == h)
-                    size.push_back(temp);
-            }
-        }
-        temp = 0;
-    }*/
+   
     length = citations.size();
     for (int i = 0; i < citations.size(); i++) {
         front = 0;
-        h = citations[i];
+        h++;
         end = length;
         for (int j = 0; j < citations.size(); j++) {
             if (h > citations[j])
@@ -50,7 +38,7 @@ int solution(vector<int> citations) {
 }
 
 int main() {
-    vector<int>citations = { 3, 0, 6, 1, 5 };
+    vector<int>citations = { 4, 0, 6, 1, 5 };
     sort(citations.begin(), citations.end());
     cout << solution(citations) << endl;
     
