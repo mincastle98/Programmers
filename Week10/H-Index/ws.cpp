7bc331e9172701450dfa560ea68957926574a933
WoosungMichael <woosung0420k@naver.com>
2021-01-04 17:38:56 +0900

    21/01/04 solved H-Index -ws

---
+++ Week10/H-Index/ws.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> citations) {
+    int answer = 0;
+    sort(citations.begin(), citations.end());
+    for (int i = 1; i <= citations.size(); i++) {
+        if (i <= citations[citations.size() - i])
+            answer = i;
+        else
+            break;
+    }
+    return answer;
+}
