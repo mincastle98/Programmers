826b71084cf4b33ebac4d24ebbc4fa70949efe33
mincastle98 <alstjd98522@naver.com>
2021-01-04 18:25:09 +0900

    21/01/04 solved H-Index - ms

--- Week10/H-Index/ms.cpp
+++ Week10/H-Index/ms.cpp
@@ -8,17 +8,10 @@ int solution(vector<int> citations) {
     int answer = 0;
     sort(citations.begin(), citations.end());
 
-    for (int i = 0; i < citations.size(); i++) {
-        if (i > 1 && citations[i] == citations[i - 1]) continue;
-        if (i > citations[i] || citations.size() - i < citations[i]) continue;
-
-        else if (i > answer) answer = citations[i];
+    for (int i = 1; i <= citations.size(); i++) {
+        if (i <= citations[citations.size() - i]) answer = i;
+        else break;
     }
 
     return answer;
-}
-
-int main(void) {
-    solution({0, 0, 0, 0, 0, 0, 2, 2});
-    return 0;
 }
