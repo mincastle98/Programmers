e64bb40545c50658aab77a0ecb6d341279f1e7af
dongyeon-0822 <myjjue00@gmail.com>
2021-01-04 17:48:23 +0900

    Update dong.cpp

---
+++ Week10/H-Index/dong.cpp
@@ -0,0 +1,25 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int solution(vector<int> citations) {
+    int answer = 0;
+    
+    sort(citations.begin(), citations.end());
+    int max = citations.back();
+    
+    int h_index = 0;
+    for (int h_index = 0; h_index <= max; h_index++) {
+        int count = 0;
+        for (int i = 0; i < citations.size(); i++) {
+            if (citations[i] >= h_index)
+                count++;
+        }
+        if (h_index == count) {
+            answer = h_index;
+        }
+    }
+    return answer;
+}
+\
