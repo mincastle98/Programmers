a03943cece01ba6d2b2815d1d87e1d9338862c14
mincastle98 <alstjd98522@naver.com>
2021-01-04 18:02:07 +0900

    21/01/04 solving H-Index - ms

--- Week10/H-Index/ms.cpp
+++ Week10/H-Index/ms.cpp
@@ -7,21 +7,18 @@ using namespace std;
 int solution(vector<int> citations) {
     int answer = 0;
     sort(citations.begin(), citations.end());
-    //citations.erase(unique(citations.begin(), citations.end()), citations.begin());
 
-    for (int i : citations) {
-        if (i == answer) continue;
-        auto iter = find(citations.begin(), citations.end(), i);
-        int idx = iter - citations.begin();
+    for (int i = 0; i < citations.size(); i++) {
+        if (i > 1 && citations[i] == citations[i - 1]) continue;
+        if (i > citations[i] || citations.size() - i < citations[i]) continue;
 
-        if (idx > i || citations.size() - idx < i) continue;
-        else if (i > answer) answer = i;
+        else if (i > answer) answer = citations[i];
     }
 
     return answer;
 }
 
 int main(void) {
-    solution({3, 0, 6, 1, 5});
+    solution({0, 0, 0, 0, 0, 0, 2, 2});
     return 0;
 }
