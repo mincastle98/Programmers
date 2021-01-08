5341978aca2b2f8f7bc64b957098ebd82d5f2a0f
sua1223 <70890638+sua1223@users.noreply.github.com>
2021-01-08 22:12:11 +0900

    21/01/08 solved LifeBoat - sua

--- Week10/LifeBoat/sua.cpp
+++ Week10/LifeBoat/sua.cpp
@@ -9,20 +9,15 @@ int solution(vector<int> people, int limit) {
 
 	sort(people.begin(), people.end());
 
-	while (!people.empty()) {
-		int weight = 0;
-
-		weight += people[0];
-		people.erase(people.begin());
-
-		for (int i = people.size() - 1; i >= 0; i--) {
-			if (people[i] <= limit - weight) {
-				weight += people[i];
-				people.erase(people.begin() + i);
-				i--;
-			}
-			if (limit - weight < people[0])
-				break;
+	int start = 0;
+	int end = people.size()-1;
+	
+	while (start <= end) {
+		if (people[end] + people[start] > limit)
+			end--;
+		else {
+			start++;
+			end--;
 		}
 		answer++;
 	}
@@ -31,7 +26,7 @@ int solution(vector<int> people, int limit) {
 }
 
 int main() {
-	vector<int> people = { 70,50,80,50 };
+	vector<int> people = { 70,80,50 };
 	int limit = 100;
 
 	cout << solution(people, limit)<<endl;
