f988257f4eef7becc8e0c5d93b141c10aefab282
HyewonKkang <68578916+HyewonKkang@users.noreply.github.com>
2021-01-08 21:04:59 +0900

    21/01/08 solved LifeBoat - hyewon

--- Week10/LifeBoat/hyewon.py
+++ Week10/LifeBoat/hyewon.py
@@ -1,13 +1,15 @@
+from collections import deque
 def solution(people, limit):
     answer = 0
     people.sort()
+    people = deque(people)
     while len(people) != 0:
         if len(people) == 1:
             answer += 1
             break
         else:
             if people[0] + people.pop() <= limit:
-                people.pop(0)
+                people.popleft()
         answer += 1
 
     return answer
