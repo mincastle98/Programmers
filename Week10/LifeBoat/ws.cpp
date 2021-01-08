200ef33299cffc631e70d45ba9dc364fed3baa38
WoosungMichael <woosung0420k@naver.com>
2021-01-08 21:14:27 +0900

    solving -ws

--- Week10/LifeBoat/ws.cpp
+++ Week10/LifeBoat/ws.cpp
@@ -7,23 +7,15 @@ using namespace std;
 int solution(vector<int> people, int limit) {
     int answer = 0;
     sort(people.begin(), people.end());
-    for (int i = people.size() - 1; i > 0; i--) {
+    for (int i = people.size() - 1; i >= 0; i--) {
         if (people[i] + people[0] <= limit) {
-            for (int j = i - 1; j >= 0; j--) {
-                if (people[i] + people[j] <= limit) {
-                    people.erase(people.begin() + i);
-                    people.erase(people.begin() + j);
-                    i--;
-                    answer++;
-                    break;
-                }
-            }
+            people.erase(people.begin());
+            i--;
+            answer++;
         }
         else {
-            people.erase(people.begin() + i);
             answer++;
         }
     }
-    answer += people.size();
     return answer;
 }
