3d9fefd00eeb4d33e9129cf28ab32617c781ccb3
Choiyunh_ <svs101@naver.com>
2021-01-08 20:13:21 +0900

    modify isPrime function

--- Week10/H-Index/uno.py
+++ Week10/H-Index/uno.py
@@ -1,10 +1,24 @@
-def solution(citations):
-    num = len(citations)
-    citations.sort()
-    for i in range(num):
-        if citations[i] >= num - i:
-            return num - i
-    return 0
+from itertools import combinations
+import math
+
+def isPrime(num):
+    for i in range(2, (int)(math.sqrt(num)) + 1):
+        if (num % i == 0):
+            return False
+    return True
 
 
-print(solution([3, 0, 1, 6, 5]))
+def solution(nums):
+    answer = 0
+    comList = list(combinations(nums, 3))
+
+    for item in comList:
+        if (isPrime(sum(item))):
+            answer += 1
+    return answer
+
+
+# 5C3 -> 5*4*3*2*1/3*2*1 * 1/2
+
+
+print(solution([1, 2, 7, 6, 4]))
